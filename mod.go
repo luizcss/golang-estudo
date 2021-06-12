0a1f72817c78a4bbb173e80e030601a90c9c4b3d
Luiz Cláudio <lzcdoss@gmail.com>
2021-06-12 02:05:51 -0300

    Update mod.go

--- mod.go
+++ mod.go
@@ -1,5 +1,13 @@
-module helloworld
+module estudo-golang
 
 go 1.15
 
-require github.com/gin-gonic/gin v1.7.2 
+require (
+	github.com/gin-gonic/gin v1.7.2
+	github.com/kr/fs v0.1.0 // indirect
+	github.com/kr/pretty v0.2.1 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/tools/godep v0.0.0-20180126220526-ce0bfadeb516 // indirect
+	golang.org/x/sys v0.0.0-20210611083646-a4fc73990273 // indirect
+	golang.org/x/tools v0.1.3 // indirect
+)
