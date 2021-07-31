695087d5b7f62642d6fdb2dbceb5089701797c0a
Luiz Cláudio <lzcdoss@gmail.com>
2021-07-30 21:32:32 -0300

    Update mod.go

--- mod.go
+++ mod.go
@@ -2,12 +2,4 @@ module estudo-golang
 
 go 1.15
 
-require (
-	github.com/gin-gonic/gin v1.7.2
-	github.com/kr/fs v0.1.0 // indirect
-	github.com/kr/pretty v0.2.1 // indirect
-	github.com/kr/text v0.2.0 // indirect
-	github.com/tools/godep v0.0.0-20180126220526-ce0bfadeb516 // indirect
-	golang.org/x/sys v0.0.0-20210611083646-a4fc73990273 // indirect
-	golang.org/x/tools v0.1.3 // indirect
-)
+require github.com/gofiber/fiber/v2 v2.16.0
